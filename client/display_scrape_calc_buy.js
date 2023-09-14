906c219cb09848ddb5304162b56b8e193f82642b
George <gmittendorf@gmail.com>
2023-09-14 00:25:35 -0700

    wait 5 sec, then fetch api

--- client/display_scrape_calc_buy.js
+++ client/display_scrape_calc_buy.js
@@ -1,11 +1,20 @@
-fetch("/api/scrape_calculate_buy")
-.then((response) => response.text())
-.then((data) => {
-    const balanceElement = document.getElementById("scrape_calculate_buy");
-    balanceElement.textContent = data;
-})
-.catch((error) => {
-    console.error("Error fetching scrape_calculate_buy: ", error);
-    const balanceElement = document.getElementById("scrape_calculate_buy");
-    balanceElement.textContent = "Error fetching scrape_calculate_buy";
-});
+
+
+
+setTimeout(() => {
+    console.log("5 seconds have passed!");
+
+    fetch("/api/scrape_calculate_buy")
+    .then((response) => response.text())
+    .then((data) => {
+        const balanceElement = document.getElementById("scrape_calculate_buy");
+        balanceElement.textContent = data;
+    })
+    .catch((error) => {
+        console.error("Error fetching scrape_calculate_buy: ", error);
+        const balanceElement = document.getElementById("scrape_calculate_buy");
+        balanceElement.textContent = "Error fetching scrape_calculate_buy";
+    });
+
+  }, 5000);
+  
